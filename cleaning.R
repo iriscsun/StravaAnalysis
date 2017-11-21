ee75e41348426fa53c5b3d57ac2a8ec2441045cd
iriscsun <iris.sun@nike.com>
2017-11-21 13:48:27 -0800

    created a new column for ratio between moving and elapsed time

--- cleaning.R
+++ cleaning.R
@@ -12,15 +12,28 @@ data$location_country[data$location_country == "The Netherlands" |
                       data$location_country == "Nederland"] <- "Netherlands"
 data$location_country[data$location_country == "Brasil"] <- "Brazil"
 
+#find top 5 countries by number of entries
+top5 <- select(data, location_country) %>%
+        group_by(location_country) %>%
+        summarise(numEntries = n()) %>%
+        arrange(desc(numEntries))
+
 #removed rows that had an elapsed time of 0
 data <- filter(data, elapsed_time > 0)
 
 #creating dataframe with cleaned data
 cleaned.data <- select(data, location_country, average_watts, type, average_heartrate, max_heartrate, 
                        moving_time, elapsed_time) %>%
+                
+                #use rows only from location_country of top 5 countries by entries
                 filter(location_country == "United States" | location_country == "Australia" |
                        location_country == "United Kingdom" | location_country == "Netherlands" |
-                       location_country == "Brazil")
+                       location_country == "Brazil") %>%
   
+                #creating new column to calculate a ratio between moving and elapsed time
+                mutate(activeRatio = moving_time/elapsed_time)
+                
+  
+#export dataframe as csv
 write.csv(cleaned.data, file="cleanedData.csv", row.names = FALSE)
 
