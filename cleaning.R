d0341ecd635c2ec42a233a0f53aec5b7675559e9
iriscsun <iris.sun@nike.com>
2017-11-21 14:58:43 -0800

    updated features in dataframes for each question

--- cleaning.R
+++ cleaning.R
@@ -12,18 +12,30 @@ data$location_country[data$location_country == "The Netherlands" |
                       data$location_country == "Nederland"] <- "Netherlands"
 data$location_country[data$location_country == "Brasil"] <- "Brazil"
 
-#find top 5 countries by number of entries
-top5 <- select(data, location_country) %>%
-        group_by(location_country) %>%
-        summarise(numEntries = n()) %>%
-        arrange(desc(numEntries))
-
 #removed rows that had an elapsed time of 0
 data <- filter(data, elapsed_time > 0)
 
-#creating dataframe with cleaned data
-cleaned.data <- select(data, location_country, average_watts, type, average_heartrate, max_heartrate, 
-                       moving_time, elapsed_time) %>%
+#find top 2 exercise types by number of entries
+top2 <- select(data, type) %>%
+  group_by(type) %>%
+  summarise(numEntries = n()) %>%
+  arrange(desc(numEntries)) 
+
+#find top 5 countries by number of entries
+top5 <- select(data, location_country) %>%
+  group_by(location_country) %>%
+  summarise(numEntries = n()) %>%
+  arrange(desc(numEntries)) 
+
+#creating dataframe for question 1 with cleaned data
+cleaned.dataQ1 <- select(data, athlete.sex, average_speed, max_speed, distance, 
+                         average_heartrate, max_heartrate, type) %>%
+  
+                  #use rows only from type of top 2 types by entries
+                  filter(type == "Ride" | type == "Run")
+
+#creating dataframe for question 2 with cleaned data
+cleaned.dataQ2 <- select(data, location_country, average_watts, moving_time, elapsed_time, type) %>%
                 
                 #use rows only from location_country of top 5 countries by entries
                 filter(location_country == "United States" | location_country == "Australia" |
@@ -32,8 +44,8 @@ cleaned.data <- select(data, location_country, average_watts, type, average_hear
   
                 #creating new column to calculate a ratio between moving and elapsed time
                 mutate(activeRatio = moving_time/elapsed_time)
-                
-  
-#export dataframe as csv
-write.csv(cleaned.data, file="cleanedData.csv", row.names = FALSE)
+
+#export dataframes as csv
+write.csv(cleaned.dataQ1, file="cleanedDataQ1.csv", row.names = FALSE)
+write.csv(cleaned.dataQ2, file="cleanedDataQ2.csv", row.names = FALSE)
 
